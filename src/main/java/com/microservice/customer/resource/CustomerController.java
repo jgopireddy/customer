8f32d4414a7fc8582db861887bbd0ea4a9656d15
JAGJEEVAN GOPIREDDY <jgopireddy@JAGJEEVANs-MacBook-Pro.local>
2020-07-18 21:03:33 -0400

    updates

--- src/main/java/com/microservice/customer/resource/CustomerController.java
+++ src/main/java/com/microservice/customer/resource/CustomerController.java
@@ -1,5 +1,10 @@
 package com.microservice.customer.resource;
 
+import java.util.Enumeration;
+
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -9,7 +14,13 @@ import org.springframework.web.bind.annotation.RestController;
 public class CustomerController {
 
 	@GetMapping("/message")
-	public String test() {
+	public String test(HttpServletRequest request, HttpServletResponse response) {
+		Enumeration<String> headers = request.getHeaderNames();
+		while(headers.hasMoreElements()) {
+			String headerKey = headers.nextElement();
+			String headerValue = request.getHeader(headerKey);
+			System.out.println(headerKey +" : "+headerValue);
+		}
 		return "Hello from Customer Service";
 	}
 }
